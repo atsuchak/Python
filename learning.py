61b51ec337360072a32f2c41efa267f273a73d63
atsuchak <suchak0630@gmail.com>
2024-08-25 00:58:12 +0600

    remove the comment

--- learning.py
+++ learning.py
@@ -3,7 +3,7 @@
 
 # some strings where name, food, email are variables. we need to use double quote to write a string variable
 
-''' name = "suchak" 
+name = "suchak" 
 food = "pizza"
 email = "suchak0630@gmail.com"
 
@@ -19,7 +19,7 @@ Age = 21
 weight = 80
 population_of_bd = 200000000
 
-print(f"Hello Mr. {name}. "f"For conferming your {food} "f"I need your email. "f"is {email} is correct? "f"I saw that you are {Age} years old "f"and your weight is {weight} which is overweight. You should avoid pizza.") '''
+print(f"Hello Mr. {name}. "f"For conferming your {food} "f"I need your email. "f"is {email} is correct? "f"I saw that you are {Age} years old "f"and your weight is {weight} which is overweight. You should avoid pizza.")
 
 # some floats. floats is not a integer number
 
