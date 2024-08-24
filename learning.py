a4d6174bb10f6cadb61f5d3c6176d91b75f84dff
atsuchak <suchak0630@gmail.com>
2024-08-25 00:54:01 +0600

    Learn python

---
+++ learning.py
@@ -0,0 +1,30 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable as if it was the value it contains
+
+# some strings where name, food, email are variables. we need to use double quote to write a string variable
+
+''' name = "suchak" 
+food = "pizza"
+email = "suchak0630@gmail.com"
+
+print(name) # print variable
+
+print(f"Hello {name}") # if i want to add another word in a print statement then we can use f and then the word and last the variable
+
+print(f"If you like {food} "f"then you are {name}. "f"and your email will be {email}")
+
+# some integers. Integers no need any quote
+
+Age = 21
+weight = 80
+population_of_bd = 200000000
+
+print(f"Hello Mr. {name}. "f"For conferming your {food} "f"I need your email. "f"is {email} is correct? "f"I saw that you are {Age} years old "f"and your weight is {weight} which is overweight. You should avoid pizza.") '''
+
+# some floats. floats is not a integer number
+
+price = 14.99
+height = 5.7
+weight = 48.35
+
+print(f"the price of the banana is {price}$. "f"Your height {height}?")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(f"if you like {food}the you are {name}")
